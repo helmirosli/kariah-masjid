8513c6734153dfb18fe579721899a0b72631aeb8
helmirosli <helmirosli@gmail.com>
2017-05-30 01:10:13 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => database_path('/../kariah_masjid.sqlite'),
+            'database' => database_path('kariah_masjid.sqlite'),
             'prefix' => '',
         ],
 
